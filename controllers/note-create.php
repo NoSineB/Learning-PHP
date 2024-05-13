44c2e3ab73cdb3811c72c4edb013232432343278
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-13 11:42:58 +0530

    Form Validation Added

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,11 +6,26 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 if($_SERVER["REQUEST_METHOD"]== "POST"){
-    $db->query("INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES (DEFAULT, :body, :user_id);", [
-        "body"=>$_POST["body"],
-        "user_id"=>1
-    ]);
-    return routeToController('/notes', $routes);
+
+    $errors = [];
+
+    if(strlen($_POST["body"]) == 0){
+        $errors["body"] = "A Body is needed";
+    }
+
+    if(strlen($_POST["body"]) >10){
+        $errors["body"] = "Body Characters execeds the limit 1000";        
+    }
+
+    if(empty($errors)){
+        $db->query("INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES (DEFAULT, :body, :user_id);", [
+            "body"=>$_POST["body"],
+            "user_id"=>1
+        ]);
+        return routeToController('/notes', $routes);
+    }
+
+    
 }
 
 require("views/note-create.view.php");
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -6,15 +6,26 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <form action="" class="flex flex-col" method="POST">
             <div class="mb-4">
-                <label for="body">Description</label>
+                <label for="body">Body</label>
             </div>
-            <div class="mb-4">
-                <textarea name="body" id="body" cols="100" rows="5" class="p-2"></textarea>
+            <div class="mb-2">
+                <textarea 
+                    name="body" 
+                    id="body" 
+                    cols="100" 
+                    rows="5" 
+                    class="p-2" 
+                    placeholder="Enter the body of the note ....." 
+                ><?= $_POST['body'] ?? '' ?></textarea>
             </div>
-            <div class="mb-4">
+            <div class="mb-2 text-xs">
+                <p class="text-red-500"><?= $errors["body"] ?? '' ?></p>
+            </div>
+            <div class="mt-2">
                 <button type="submit" class="px-2 py-1 text-green-500 border-2 border-green-500 rounded-md hover:text-white hover:bg-green-500 duration-500">Submit</button>
             </div>
         </form>
+        
     </div>
 </main>
 
