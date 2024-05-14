1f58ff055929d0f77055aa70e6975a2cd4afb9a5
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-14 09:05:34 +0530

    Error Rectified

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -14,23 +14,23 @@ $db = new Database($config['database']);
 $errors = [];
 
 
-if($_SERVER["REQUEST_METHOD"]== "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if(! Validator::string($_POST["body"], 1, 1000)){
+    if (!Validator::string($_POST["body"], 1, 1000)) {
         $errors['body'] = "Enter Body not more than 1000 characters";
     }
 
-    if(empty($errors)){
+    if (empty($errors)) {
         $db->query("INSERT INTO `notes` (`id`, `body`, `user_id`) VALUES (DEFAULT, :body, :user_id);", [
-            "body"=>$_POST["body"],
-            "user_id"=>1
+            "body" => $_POST["body"],
+            "user_id" => 1
         ]);
-        
-        header('/routes');
+
+        header('location: /notes');
         exit();
     }
 
-    
+
 }
 
 view('notes/create.view.php', [
