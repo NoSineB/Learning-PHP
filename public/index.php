ac2e8fdc0d720970f900524cb58740cd29566a1e
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-17 11:29:24 +0530

    Composer and composer autoload Added

--- public/index.php
+++ public/index.php
@@ -7,14 +7,9 @@ session_start();
 
 const BASE_DIR = __DIR__.'/../';
 
+require BASE_DIR . "vendor/autoload.php";
+
 require BASE_DIR."Core/functions.php";
-
-spl_autoload_register(function($class){
-    $class = str_replace("\\", DIRECTORY_SEPARATOR , $class);
-
-    require base_dir("{$class}.php");
-});
-
 
 require base_dir("bootstrap.php");
 
